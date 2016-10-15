f63ca87e3d662bc7e8fc88cfa25136b19ee17e28
Budagala <budagala.ashok16@gmail.com>
2016-10-15 20:41:28 +0530

    added the testbase class

---
+++ src/test/java/com/alt/sundaykart/automation/base/TestData.java
@@ -0,0 +1,8 @@
+package com.alt.sundaykart.automation.base;
+
+public class TestData {
+	
+	public static final String username="saihari521@gmail.com";
+	public static final String password="sai";
+
+}
